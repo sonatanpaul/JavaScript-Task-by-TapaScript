510c3fb37485e91c9dc3c3627d8625b0cae1dc3b
sonatanpaul <iamsonatanpaul@gmail.com>
2025-03-08 19:58:17 +0600

    solve task day 5

--- Task-05/app.js
+++ Task-05/app.js
@@ -1,1 +1,58 @@
-console.log("hello world");
+// 1. Generate a Pyramid Pattern using Nested Loop as it is shown below:
+
+for (let i = 1; i <= 5; i++) {
+  //   console.log(i);
+  let star = "";
+  for (let j = 1; j <= i; j++) {
+    star = star + "*";
+  }
+  //   console.log(star);
+}
+
+// reverse way
+
+for (let i = 5; i >= 1; i--) {
+  //   console.log(i);
+  let star = "";
+  for (let j = i; j >= 1; j--) {
+    // console.log(j);
+    star = star + "*";
+  }
+  //   console.log(star);
+}
+
+// 2. Craete Multiplication Table (Using for loop)
+let count = 3;
+for (let i = 1; i <= 10; i++) {
+  //   console.log(i);
+  let multi = count + "X" + i + "=" + count * i;
+  console.log(multi);
+}
+
+// 4. Skipping Multiples of 3
+
+for (let i = 1; i <= 20; i++) {
+  if (i % 3 === 0) {
+    continue;
+  }
+  //   console.log(i);
+}
+
+// 5. Reverse Digits of a Number (Using while loop)
+
+let number = 6789;
+console.log(number.length);
+for (let i = 5; i >= 1; i--) {
+  console.log(number[i]);
+}
+
+for (initialize - variable; conditon; Update) {
+  // code
+}
+
+let num = 1;
+
+do {
+  console.log(num);
+  num++;
+} while (num <= 5);
---
+++ Task-05/readme.md
@@ -0,0 +1,25 @@
+# Write your understanding on the difefrences between for, while, and do-while loop. Create their flow charts.
+
+# 01. For Loop
+
+- For loop is best when we know exactly how many times we need to run a block of code .
+
+### Flow Chart :
+
+![for-loop](./images/for%20loop.png)
+
+# 02 . While Loop
+
+-A while loop runs as long as a given condition is true . It’s best when we don’t know in advance how many iterations are needed .
+
+### Flow Chart :
+
+![while-lop](./images/while%20loop.png)
+
+# 03. Do-While-Lop
+
+- A do while loop ensures that the code executes at least once before checking the condition .
+
+### Flow Char :
+
+![do-while-loop](./images/do-while.png)
