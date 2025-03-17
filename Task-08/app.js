ef0cdeb40840bde35790fa6fcdd43a3094d9ebf9
sonatanpaul <iamsonatanpaul@gmail.com>
2025-03-17 17:44:09 +0600

    task day -8 solve and readme file add

---
+++ Task-08/app.js
@@ -0,0 +1,20 @@
+const message = "I can do it";
+
+function sum(a, b) {
+  const result = a + b;
+  return result;
+}
+
+function mul(a, b) {
+  const result = a * b;
+  return result;
+}
+function calc(a, b) {
+  return (sum(a, b) + mul(a, b)) / 2;
+}
+
+function getResult(a, b) {
+  return calc(a, b);
+}
+
+getResult(8, 5);
