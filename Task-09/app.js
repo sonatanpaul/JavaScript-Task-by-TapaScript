8cbf035a3943f62033701556856e04ef81f99e63
sonatanpaul <iamsonatanpaul@gmail.com>
2025-03-17 22:59:01 +0600

    problem solve task 9

--- Task-09/app.js
+++ Task-09/app.js
@@ -1,6 +1,5 @@
 // Expian Temporal Dead Zone by creating 3 variables in side a block. Post the code as your answer.
 
-
 {
   // name, friendName, FavoriteColor TDZ zone started
   //
@@ -23,3 +22,58 @@
   let FavoriteColor = "Black"; // FavoriteColor TDZ zone end
   //   console.log(FavoriteColor); // Black // No Error
 }
+
+// Explain Variable and Function Hoisting with Example. Post the code as your answer.
+/*
+ Hoisting means the creation phase in the execution context
+be a global execution context or function execution context
+when javascript create the memory of a variable or
+function an initilaze or if possible initilazition them
+why because if it is variable then call variable hoisting
+if it is function then call function hoisting */
+
+// var keyword
+console.log("name is", names); // undifined
+var names;
+names = "Sonatan Paul";
+console.log("name is", names); // Sonatan Paul
+
+// let keyword
+
+console.log("friend name is", friendName); // Reference Error
+let friendName;
+friendName = "Anik";
+console.log("Friend Name is", friendName); // Anik
+
+// const keyword
+
+console.log("Favorite Color is", color); // Reference Error
+const color = "Black";
+console.log("Favorite Color is", color); // Color
+
+// function hoisting
+
+// invoke function, sayName
+sayName();
+
+// declare function sayName
+function sayName() {
+  console.log("My name is Sonatan Paul");
+  // invoke function myFriendName
+  myFrindName();
+}
+
+// function declare myFriendName
+function myFrindName() {
+  console.log("My friend Name is Anik");
+}
+
+// Function Hoisting as a Expression
+
+// invoke function
+fullName(); // fullName is not a function reasson fullName is variable and varibale value is function
+
+// declare function fullName
+var fullName = function () {
+  console.log("My full Name is Sonatan Paul");
+};
