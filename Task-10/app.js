e655bac6e700a4204a630ed257f9286603b1a8fb
sonatanpaul <iamsonatanpaul@gmail.com>
2025-03-18 14:16:27 +0600

    day task solve done

--- Task-10/app.js
+++ Task-10/app.js
@@ -26,8 +26,8 @@ console.log(total);
 // 3. Create a function with a nested function and log a variable from the parent function.
 
 function parent() {
-  let parentName = "Kartik Chandra Paul";
   function child() {
+    let parentName = "Kartik Chandra Paul";
     console.log(parentName);
   }
   child();
@@ -35,7 +35,7 @@ function parent() {
 parent();
 
 // Ans : Kartik Chandra Paul
-
+// 4. Use a loop inside a function and declare a variable inside the loop. Can you access it outside?
 function sayName() {
   for (let i = 0; i <= 10; i++) {
     let names = "Sonata Paul";
@@ -45,3 +45,80 @@ sayName();
 
 // console.log(names); // Reference Error
 //  Resson : when we declare {} using let and const variable cant access outside
+
+// 5. Write a function that tries to access a variable declared inside another function.
+function outer() {
+  let color = "Black";
+  function inner() {
+    console.log(color);
+  }
+  inner();
+}
+outer();
+
+// 6. What will be the output and why?
+// console.log(a); // Reference Error
+// let a = 10;
+
+// resson  we can't access let and const variable before initialization . and execution context e crateion phase e hoisting hoi but variable undifined set hoi nah ;
+
+// 7. Where is the age variable accessible?
+function showAge() {
+  let age = 25;
+  console.log(age); // only this console log accessible
+}
+
+// console.log(age); // ReferenceError
+// resson : we can't access inside variable outside
+// when showAge Call answer is B: Only inside showAge
+
+// 8. What will be the output and explain the output?
+let message = "Hello";
+
+function outer() {
+  let message = "Hi";
+
+  function inner() {
+    console.log(message);
+  }
+
+  inner();
+}
+
+outer();
+// Ans : Hi
+// Resson : when we declare global variable and inside function variable first piority is inside function variable when function variable not available then access global variable ,, this is call variable shadowing
+
+// 9. What will be the output and why?
+let x = "Global";
+
+function outer() {
+  let x = "Outer";
+
+  function inner() {
+    let x = "Inner";
+    console.log(x);
+  }
+
+  inner();
+}
+
+outer();
+
+// Ans : Inner
+// resson : when we declare global variable  then  function varable then or nested function variable first access nested variable  if not available then  outer function variable if not available then access global variable
+
+// 10. What will be the output and why?
+function counter() {
+  let count = 0;
+  return function () {
+    count--;
+    console.log(count);
+  };
+}
+
+const reduce = counter();
+reduce(); // -1
+reduce(); // -2
+
+// reasson : when we declare or return function inside onother function inner fuction acessalbe any variable of outer function and use them
