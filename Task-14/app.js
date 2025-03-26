3954cad3c8c675ed614f861fddb3ea2ca530dbeb
sonatanpaul <iamsonatanpaul@gmail.com>
2025-03-27 00:51:41 +0600

    task one is complete

---
+++ Task-14/app.js
@@ -0,0 +1,17 @@
+console.log("Hello WOrld");
+// 1. What will be the output of the following code?
+try {
+  let r = p + 50;
+  console.log(r);
+} catch (error) {
+  console.log("An error occurred:", error.name);
+}
+// ReferenceError
+// reason :  p is not defiend thats why p can't accessible
+
+
+// 2. Write a function processPayment(amount) that checks if the amount is positive and not exceeding balance. If any condition fails, throw appropriate errors
+
+function processPayment(amount){
+    0
+}
